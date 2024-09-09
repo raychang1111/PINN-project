5a831f3357e6bc9c4aa855c3e3cfab5e2ed48fe2
raychang1111 <100055927+raychang1111@users.noreply.github.com>
2024-09-09 22:12:39 +0800

    Add files via upload

---
+++ pinn__poisson_and_inverse_1_test1.py
@@ -0,0 +1,368 @@
+import time
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import qmc
+torch.set_default_dtype(torch.float64)
+
+def tru(x, y):
+    tru=np.exp(x*x+y*y)
+    return tru
+
+def trudx(x, y):
+    trudx=np.exp(x*x+y*y)*2*x
+    return trudx
+
+def trudy(x, y):
+    trudy=np.exp(x*x+y*y)*2*y
+    return trudy
+k0=4
+def f(x,y,mu):
+  tru = mu*(x*x+y*y+1)*torch.exp(x*x+y*y)
+  return tru
+
+# 定義一個全連接神經網絡
+class FCN(nn.Module):
+  "Defines a standard fully-connected network in PyTorch"
+
+  def __init__(self, N_INPUT, N_OUTPUT, N_HIDDEN, N_LAYERS):
+    super().__init__()
+    activation = nn.Tanh        # 激活函數
+    self.fcs = nn.Sequential(*[nn.Linear(N_INPUT, N_HIDDEN),activation()])
+    # 重複 N_LAYERS - 1 次，以創建多個隱藏層
+    self.fch = nn.Sequential(*[nn.Sequential(*[nn.Linear(N_HIDDEN, N_HIDDEN),activation()]) for _ in range(N_LAYERS-1)])
+    self.fce = nn.Linear(N_HIDDEN, N_OUTPUT)
+
+  def forward(self, x):
+    x = self.fcs(x)
+    x = self.fch(x)
+    x = self.fce(x)
+    return x
+
+### data generation .......
+n_bc = 4
+n_data_per_bc = 30
+
+# Define the datapoints ............;
+engine = qmc.LatinHypercube(d=1)
+data = np.zeros([n_bc ,n_data_per_bc , 5])
+#data[i][j][0] 第i條邊中的第j個點的x座標
+#data[i][j][1] 第i條邊中的第j個點的y座標
+#data[i][j][2] 第i條邊中的第j個點的邊界值  tru(x,y)
+#data[i][j][3] 第i條邊中的第j個點的對x導數  trudx(x,y)
+#data[i][j][4] 第i條邊中的第j個點的對y導數  trudy(x,y)
+
+for i, j in zip(range(n_bc), [0., 1., 0, 1.]):
+    points = (engine.random(n=n_data_per_bc)[:, 0] - 0.) * 1
+
+    if i < 2:
+        data[i, :, 0] = j
+        data[i, :, 1] = points
+    else:
+        data[i, :, 0] = points
+        data[i, :, 1] = j
+
+
+
+# Values of data , derivative/x , and derivative/y ...
+for j in range(0,n_data_per_bc):
+    # bord x = 0
+    # 計算Dirichlet problem 邊界問題
+    data[0, j, 2] = tru(data[0, j, 0] ,data[0, j, 1] )
+    # 計算Neumann problems 邊界問題 (導數)
+    data[0, j, 3] = trudx(data[0, j, 0] ,data[0, j, 1] )
+    data[0, j, 4] = trudy(data[0, j, 0] ,data[0, j, 1] )
+
+    # bord x = 1
+    data[1, j, 2] = tru(data[1, j, 0] ,data[1, j, 1] )
+    data[1, j, 3] = trudx(data[1, j, 0] ,data[1, j, 1] )
+    data[1, j, 4] = trudy(data[1, j, 0] ,data[1, j, 1] )
+
+    # bord y = 0
+    data[2,j,2] = tru(data[2, j, 0] ,data[2, j, 1] )
+    data[2,j,3] = trudx(data[2, j, 0] ,data[2, j, 1] )
+    data[2,j,4] = trudy(data[2, j, 0] ,data[2, j, 1] )
+
+    # bord y = 1
+    data[3,j,2] = tru(data[3, j, 0] ,data[3, j, 1] )
+    data[3,j,3] = trudx(data[3, j, 0] ,data[3, j, 1] )
+    data[3,j,4] = trudy(data[3, j, 0] ,data[3, j, 1] )
+
+
+data = data.reshape(n_data_per_bc * n_bc, 5)
+
+# t_d are data values, t_dx and td_y derivatives wrt x and y respectively .....
+x_d, y_d, t_d, t_dx, t_dy = map(lambda x: np.expand_dims(x, axis=1),
+                    [data[:, 0], data[:, 1], data[:, 2], data[:, 3], data[:, 4] ])
+
+Nc = 500
+engine = qmc.LatinHypercube(d=2)
+colloc = engine.random(n=Nc)
+colloc = 1 * (colloc -0)
+
+x_c, y_c = map(lambda x: np.expand_dims(x, axis=1),
+               [colloc[:, 0], colloc[:, 1]])
+
+plt.figure(figsize=(7, 7))
+plt.title("Boundary Data points and Collocation points",fontsize=16)
+plt.scatter(data[:,0], data[:,1], marker="x", c="k", label="BDP")
+plt.scatter(colloc[:,0], colloc[:,1], s=2, marker=".", c="r", label="CP")
+plt.xlabel("x",fontsize=16)
+plt.ylabel("y",fontsize=16)
+plt.axis("square")
+
+x_d, y_d, t_d, t_dx, t_dy, x_c, y_c  = map(lambda x: torch.tensor(x,requires_grad=True).view(-1,1),
+                             [x_d, y_d, t_d, t_dx, t_dy, x_c, y_c])
+
+torch.manual_seed(7777)
+pinn = FCN(2,1,25,6)
+
+def u_nn(x, y):
+    u = pinn(torch.cat([x, y], dim=1))  #排成[[x0,y0],
+                         #   [x1,y1]]
+    return u
+
+def uderx(x, y):
+    u = pinn(torch.cat([x, y], dim=1))
+    uderx = torch.autograd.grad(u, x, torch.ones_like(u), create_graph=True)[0]
+    return uderx
+
+def udery(x, y):
+    u = pinn(torch.cat([x, y], dim=1))
+    udery = torch.autograd.grad(u, y, torch.ones_like(u), create_graph=True)[0]
+    return udery
+
+def F_nn(x, y,mu):
+    u0 = u_nn(x,y)
+    u_x = torch.autograd.grad(u0, x, torch.ones_like(u0), create_graph=True)[0]
+    u_y = torch.autograd.grad(u0, y, torch.ones_like(u0), create_graph=True)[0]
+    u_xx = torch.autograd.grad(u_x, x, torch.ones_like(u_x), create_graph=True)[0]
+    u_yy = torch.autograd.grad(u_y, y, torch.ones_like(u_y), create_graph=True)[0]
+    # Select the residual equation ....
+    F = u_xx + u_yy - f(x,y,mu)
+    retour = torch.mean(F ** 2)
+    return retour
+
+def mse(y, y_):
+    return torch.mean((y-y_)**2)
+
+# training .....
+epochs = 80000
+mu = torch.nn.Parameter(torch.zeros(1, requires_grad=True)) #定義了一個可學習的參數mu(標量),用於估計微分方程中的未知常數
+# optimiser = torch.optim.Adam(list(pinn.parameters())+[mu],lr=1e-3) #初始化優化器(使用Adam優化算法)
+optimiser1 = torch.optim.Adam(pinn.parameters(),lr=1e-4) #初始化優化器(使用Adam優化算法)
+optimiser2 = torch.optim.Adam([mu],lr=1e-1) #初始化優化器(使用Adam優化算法)
+
+mus = []
+loss_values = np.array([])
+L_values = np.array([])
+l_values = np.array([])
+# lambda1, lambda2 = 1e-0, 3e-1 #控制不同loss的權重
+start = time.time()
+for epoch in range(epochs):
+    optimiser1.zero_grad() #将梯度清零 防止梯度值累加
+    optimiser2.zero_grad()
+    #loss on PDE
+    L = F_nn(x_c, y_c,mu)
+    #loss on data
+    T_ = u_nn(x_d, y_d)
+    l = mse(t_d, T_)
+
+    loss = L + l
+    loss.backward()   #計算加權總損失loss,對其進行反向傳播來獲得所有參數的梯度
+    optimiser1.step()   #使用優化器的step()方法更新模型參數
+    optimiser2.step()
+
+    mus.append(mu.item())
+
+    if epoch % 100 == 0 or epoch == epochs-1:
+      print(f"{epoch:5}, {loss.detach().numpy():.9f}")
+      loss_values = np.append(loss_values, loss.detach().numpy())
+      L_values = np.append(L_values, L.detach().numpy())
+      l_values = np.append(l_values, l.detach().numpy())
+
+#
+end = time.time()
+computation_time = {}
+computation_time["pinn"] = end - start
+print(f"\ncomputation time: {end-start:.3f}\n")
+#
+plt.figure()
+#plt.semilogy(loss_values, label=model.name)
+plt.semilogy(loss_values, label="Total loss")
+plt.xlabel("Epochs" r'($\times 10^2$)',fontsize=16)
+plt.legend()
+
+plt.figure()
+plt.title("Inverse problem - discover $\mu$ value")
+plt.plot(mus, label="PINN estimate", color='red')
+plt.hlines(k0, 0, len(mus), label="True value", color="tab:blue")
+plt.legend()
+plt.xlabel("Training step")
+print(f"The final predicted value of mu is =")
+print(mus[-1])
+
+plt.figure()
+plt.semilogy(l_values, label='Loss_data')
+plt.legend()
+
+plt.semilogy(L_values, label='Loss_PDE')
+plt.xlabel("Epochs" r'($\times 10^2$)',fontsize=16)
+plt.legend()
+
+n = 100
+l = 1.
+r = 2*l/(n+1)
+T = np.zeros([n*n, n*n])
+### plotting
+#plt.figure("", figsize=(16, 8))
+plt.figure(figsize=(14, 7))
+#
+X = np.linspace(0, 1, n)
+Y = np.linspace(0, 1, n)
+X0, Y0 = np.meshgrid(X, Y)
+
+X = X0.reshape([n*n, 1])
+Y = Y0.reshape([n*n, 1])
+X_T = torch.tensor(X,requires_grad=True)
+Y_T = torch.tensor(Y,requires_grad=True)
+
+# Predicted solution by the network .....
+S = u_nn(X_T, Y_T)
+S = S.detach().numpy().reshape(n, n)
+#
+
+S2=S
+plt.subplot(221)
+plt.pcolormesh(X0, Y0, S2, cmap="turbo")
+#plt.contour(X0, Y0, S2,18,linestyles='dashed',linewidths=1.5)
+plt.colorbar(pad=-0.3)
+#plt.scatter(data[:, 0], data[:, 1], marker=".", c="r", label="BDP")
+#plt.scatter(colloc[:,0], colloc[:,1], marker=".", c="b")
+plt.xlabel("X",fontsize=16)
+plt.ylabel("Y",fontsize=16)
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.title("PINN solution",fontsize=16)
+plt.tight_layout()
+plt.axis("square")
+
+#plt.show()
+#
+
+#plt.figure("", figsize=(14, 7))
+# True/exact solution to evaluate the error ...
+TT = tru(X0,Y0)
+
+TT2 = (TT - S2)
+#TT2 = (TT - S2)/TT
+
+plt.subplot(222)
+plt.pcolormesh(X0, Y0, TT2, cmap="turbo")
+#plt.contour(X0, Y0, TT2,21)
+plt.colorbar(pad=-0.3)
+plt.xlabel("X",fontsize=16)
+plt.ylabel("Y",fontsize=16)
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.title("different", fontsize=16)
+#plt.title("Relative error", fontsize=16)
+plt.tight_layout()
+plt.axis("square")
+
+fig = plt.figure(figsize=(18, 6))
+
+# 左侧子图
+ax1 = fig.add_subplot(131, projection='3d')
+surf1 = ax1.plot_surface(X0, Y0, S2, cmap='rainbow')
+ax1.set_xlabel("x", fontsize=16)
+ax1.set_ylabel("y", fontsize=16)
+ax1.set_zlabel("u(x,y)", fontsize=16)
+ax1.set_title("PINN Solution", fontsize=16)
+
+# 中間子图
+ax2 = fig.add_subplot(132, projection='3d')
+surf2 = ax2.plot_surface(X0, Y0, TT2, cmap='rainbow')
+ax2.set_xlabel("x", fontsize=16)
+ax2.set_ylabel("y", fontsize=16)
+ax2.set_zlabel("u(x,y)", fontsize=16)
+ax2.set_title("different", fontsize=16)
+
+# 右侧子图
+ax3 = fig.add_subplot(133, projection='3d')
+surf3 = ax3.plot_surface(X0, Y0, TT, cmap='rainbow')
+ax3.set_xlabel("x", fontsize=16)
+ax3.set_ylabel("y", fontsize=16)
+ax3.set_zlabel("u(x,y)", fontsize=16)
+ax3.set_title("True Solution", fontsize=16)
+
+# plot f(x,y)
+TT = f(X_T,Y_T,k0)
+TT2 = f(X_T,Y_T,mus[-1])
+TT = TT.detach().numpy().reshape(n, n)
+TT2 = TT2.detach().numpy().reshape(n, n)
+TT3 = (TT2-TT)
+
+fig = plt.figure(figsize=(21, 7))
+
+# 左侧子图
+ax1 = fig.add_subplot(131, projection='3d')
+surf1 = ax1.plot_surface(X0, Y0, TT, cmap='rainbow')
+ax1.set_xlabel("x", fontsize=16)
+ax1.set_ylabel("y", fontsize=16)
+ax1.set_zlabel("f(x,y)", fontsize=16)
+ax1.set_title("true_f", fontsize=16)
+
+# 右侧子图
+ax2 = fig.add_subplot(132, projection='3d')
+surf2 = ax2.plot_surface(X0, Y0, TT2, cmap='rainbow')
+ax2.set_xlabel("x", fontsize=16)
+ax2.set_ylabel("y", fontsize=16)
+ax2.set_zlabel("f_nn(x,y)", fontsize=16)
+ax2.set_title("PINN_f", fontsize=16)
+
+ax3 = fig.add_subplot(133, projection='3d')
+surf3 = ax3.plot_surface(X0, Y0, TT3, cmap='rainbow')
+ax3.set_xlabel("x", fontsize=16)
+ax3.set_ylabel("y", fontsize=16)
+ax3.set_zlabel("f_nn-f", fontsize=16)
+ax3.set_title("different", fontsize=16)
+
+u0 = u_nn(X_T, Y_T)
+u_x = torch.autograd.grad(u0, X_T, torch.ones_like(u0), create_graph=True)[0]
+u_y = torch.autograd.grad(u0, Y_T, torch.ones_like(u0), create_graph=True)[0]
+u_xx = torch.autograd.grad(u_x, X_T, torch.ones_like(u_x), create_graph=True)[0]
+u_yy = torch.autograd.grad(u_y, Y_T, torch.ones_like(u_y), create_graph=True)[0]
+
+
+# Select the residual equation ....
+F = u_xx + u_yy - f(X_T,Y_T,k0)
+S = F.detach().numpy().reshape(n, n)
+F1 = u_xx + u_yy - f(X_T,Y_T,mus[-1])
+S1 = F1.detach().numpy().reshape(n, n)
+
+fig = plt.figure(figsize=(14, 7))
+ax = fig.add_subplot(121, projection='3d')
+surf = ax.plot_surface(X0, Y0, S, cmap='rainbow')
+ax.set_xlabel("x", fontsize=16)
+ax.set_ylabel("y", fontsize=16)
+# ax.set_zlabel("u(x,y)", fontsize=16)
+ax.set_title("PDE loss k", fontsize=16)
+
+ax = fig.add_subplot(122, projection='3d')
+surf = ax.plot_surface(X0, Y0, S1, cmap='rainbow')
+ax.set_xlabel("x", fontsize=16)
+ax.set_ylabel("y", fontsize=16)
+# ax.set_zlabel("u(x,y)", fontsize=16)
+ax.set_title("PDE loss mu", fontsize=16)
+
+# plt.show()
+
+# 獲取所有圖像編號
+fig_nums = plt.get_fignums()
+
+# 使用迴圈保存所有圖像
+for i in fig_nums:
+    plt.figure(i)
+    plt.savefig(f'PINN poisson and inverse_1 test1 運行結果/figure_{i}.png')
---
+++ pinn_laplace_equation_3d_electromagnetism.py
@@ -0,0 +1,456 @@
+import numpy as np
+import time
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from scipy.stats import qmc
+tf.keras.backend.set_floatx("float64")
+
+v0 = 5
+a = 1
+max_r = 100
+def true_solution(r,theta,phi):
+  return v0*a/r
+# r
+# θ = Theta
+# φ = Phi
+
+n_bc = 2
+n_data_per_bc = 300
+engine = qmc.LatinHypercube(d=2)
+
+#data[i][j][0] 第i邊界面中的第j個點的r座標
+#data[i][j][1] 第i邊界面中的第j個點的Theta座標
+#data[i][j][2] 第i邊界面中的第j個點的Phi座標
+#data[i][j][3] 第i邊界面中的第j個點的邊界值
+data = np.zeros([n_bc, n_data_per_bc, 4])
+
+for i, j in zip(range(n_bc), [a,max_r]):
+  points = engine.random(n=n_data_per_bc)
+  points[:,0] = points[:,0]*np.pi
+  points[:,1] = points[:,1]*2*np.pi
+
+  data[i, :, 0] = j
+  data[i, :, 1] = points[:,0]
+  data[i, :, 2] = points[:,1]
+
+
+
+for i in range(n_bc):
+  for j in range(n_data_per_bc):
+    data[i,j,3]=true_solution(data[i,j,0],data[i,j,1],data[i,j,2])
+
+data = data.reshape(n_data_per_bc * n_bc, 4)
+
+# print(data)
+
+r_d, theta_d, phi_d ,t_d  = map(lambda x: np.expand_dims(x, axis=1),
+                    [data[:, 0], data[:, 1], data[:, 2],data[:, 3] ])
+# print(r_d)
+Nc = 600
+engine = qmc.LatinHypercube(d=3)
+
+colloc = engine.random(n=Nc)
+colloc[:,0] = colloc[:,0]*(max_r-a)+a
+colloc[:,1] = colloc[:,1]*np.pi
+colloc[:,2] = colloc[:,2]*2*np.pi
+
+# print(colloc)
+
+r_c, theta_c ,phi_c = map(lambda x: np.expand_dims(x, axis=1),
+               [colloc[:, 0], colloc[:, 1], colloc[:, 2]])
+
+#--------------------畫圖------------------------------------------------------------------------------------
+X1 = r_d * np.sin(phi_d) * np.cos(theta_d)
+Y1 = r_d * np.sin(phi_d) * np.sin(theta_d)
+Z1 = r_d * np.cos(phi_d)
+
+X2 = r_c * np.sin(phi_c) * np.cos(theta_c)
+Y2 = r_c * np.sin(phi_c) * np.sin(theta_c)
+Z2 = r_c * np.cos(phi_c)
+
+fig = plt.figure(figsize=(10, 10))
+ax = fig.add_subplot(111, projection='3d')
+
+# 绘制点点图
+ax.scatter(X1, Y1, Z1, c='b', s=10, marker='x')
+ax.scatter(X2, Y2, Z2, c='r', s=10, marker='.')
+
+# # 设置坐标轴范围
+# ax.set_xlim(-max_r, max_r)
+# ax.set_ylim(-max_r, max_r)
+# ax.set_zlim(-max_r, max_r)
+# # 设置坐标轴刻度为原始数据值
+# ax.set_xticks(np.linspace(-max_r, max_r, 11))
+# ax.set_yticks(np.linspace(-max_r, max_r, 6))
+# ax.set_zticks(np.linspace(-max_r, max_r, 6))
+
+# print(X1)
+# print("-------------------------------")
+# print(X2)
+
+# 设置坐标轴标签
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+#視圖角度
+# ax.view_init(elev=0, azim=0)
+
+# 设置图形标题
+ax.set_title('Boundary Data points and Collocation points')
+# plt.show()
+#--------------------------------------------------------------------------------------------------------
+r_c, theta_c ,phi_c, r_d, theta_d, phi_d, t_d =map(lambda x: tf.convert_to_tensor(x,dtype=tf.float64),[r_c, theta_c ,phi_c, r_d, theta_d, phi_d, t_d])
+
+# print(t_d)
+
+def DNN_builder(in_shape=3, out_shape=1, n_hidden_layers=5, neuron_per_layer=20, actfn="tanh"):
+  #使用 Keras 函數式 API 建立一個具有指定架構的全連接神經網路模型
+  # input layer 輸入層
+  input_layer = tf.keras.layers.Input(shape=(in_shape,))
+  # hidden layers 隱藏層
+  hidden = [tf.keras.layers.Dense(neuron_per_layer, activation=actfn)(input_layer)]
+  for i in range(n_hidden_layers-1):
+    new_layer = tf.keras.layers.Dense(neuron_per_layer,activation=actfn,activity_regularizer=None)(hidden[-1])
+    hidden.append(new_layer)
+  # output layer
+  output_layer = tf.keras.layers.Dense(1, activation=None)(hidden[-1])
+  # building the model
+  name = f"DNN-{n_hidden_layers}"
+  model = tf.keras.Model(input_layer, output_layer, name=name)
+  return model
+
+# 清除先前的 Tensorflow session (Session 是 TensorFlow 中執行計算的一個環境，它負責分配資源、執行操作並返回結果。)
+tf.keras.backend.clear_session()
+# 調用 DNN_builder 建立一個具有 5 個隱藏層、每層 20 個神經元 使用 tanh(雙曲正切函式) 激活函數的模型
+model = DNN_builder(3, 1, 6, 25, "tanh")##ReLU  tanh
+model.summary()
+
+
+@tf.function
+def u(r,theta,phi):
+  u = model(tf.concat([r, theta, phi], axis=1))
+  return u
+
+# ...Residual equation .. is the Laplacian here for heat equation -> Delta u = 0
+# 定義了一個函數 f(x, y),用於計算 PDE(偏微分方程)的殘差。它首先使用 u(x, y) 計算解的近似值,
+# 然後對 u 兩次求偏導以得到其二階偏導數之和,也就是 Laplace 算子 Δu。
+# 最後返回 Δu 的平方均值,作為 PDE 殘差的 loss
+@tf.function
+def f(r,theta,phi):
+  u0 = u(r,theta,phi)
+  #偏微分方程(極座標形式)
+  u_r = tf.gradients(u0, r)[0]
+  u_theta = tf.gradients(u0, theta)[0]
+  u_phi = tf.gradients(u0, phi)[0]
+
+  u_rr = tf.gradients(u_r*r*r, r)[0]
+  u_tt = tf.gradients(u_theta*tf.sin(theta), theta)[0]
+  u_pp = tf.gradients(u_phi, phi)[0]
+
+  # F = (1/(r*r))*u_rr + (1/(r*r*tf.sin(theta)))*u_tt + (1/(r*r*tf.sin(theta)*tf.sin(theta)))*u_pp
+  # F = u_rr + (1/(tf.sin(theta)))*u_tt + (1/(tf.sin(theta)*tf.sin(theta)))*u_pp
+  F = tf.sin(theta)*tf.sin(theta)*u_rr + tf.sin(theta)*u_tt + u_pp
+  # F = u_rr + u_tt + u_pp + u_theta + u_phi
+  return tf.reduce_mean(F**2)
+
+
+# 定義均方誤差 (MSE) 函數,用於計算神經網路在邊界資料點處的誤差
+@tf.function
+def mse(val, val_):
+  return tf.reduce_mean((val-val_)**2)
+
+# Training .... and print the evolution of the losses during
+# a number of epochs defined below ...
+# We use the vanilla-Pinns with soft constraints on the boundaries for Dirichlet conditions
+# ........................................................................................
+# 是訓練神經網路的主要部分。首先初始化一些變數,
+# 包括 loss 的初始值、訓練的 epoch 數、優化器和學習率設定、訓練過程中的 loss 記錄等。
+loss = 0
+epochs = 100000  #迭代次數
+# opt = tf.keras.optimizers.legacy.Adam(learning_rate=2e-4)
+
+
+# ------------------------------------------------------------------------------------
+# 定义初始学习率和衰减步骤数
+initial_learning_rate = 1e-3
+decay_steps = epochs
+alpha = 0
+
+# 使用余弦衰减学习率调度器
+lr_schedule = tf.keras.optimizers.schedules.CosineDecay(
+    initial_learning_rate=initial_learning_rate, decay_steps=decay_steps,alpha=alpha)
+opt = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+# ------------------------------------------------------------------------------------
+# opt = tf.keras.optimizers.Adam(learning_rate=2e-4)
+
+epoch = 0
+
+# 權重係數
+lambda1, lambda2 = 1e5, 1
+
+loss_values = np.array([])
+l_values = np.array([])
+L_values = np.array([])
+#
+start = time.time() #計時間
+# 訓練迴圈
+for epoch in range(epochs):
+  # print(lr_schedule(epoch).numpy())
+  # 使用 GradientTape 計算損失函數的梯度
+  with tf.GradientTape() as tape:
+    T_ = u(r_d, theta_d, phi_d)  #將邊界資訊(x,y)丟給神經網路 得到預測輸出值
+    # loss on physics - PDE
+    #將邊界內的點代入神經網路得到輸出u 在代入計算偏微分方程
+    #因為Laplace方程Δu=0(輸出值因該為0) 但實際上輸出會因為輸入u的好壞而引響不會剛好是0 故此項數值定義為PDF誤差
+    L = f(r_c, theta_c ,phi_c)
+    # loss on data
+    # 將邊界條件(c)(c是儲存邊界條件的解 ex: u(1,y)=1 ) 與預測輸出值 相減 得到data誤差 (??邊界誤差??)
+    l = mse(t_d, T_)
+    # Add the loss on data and loss on equation ....
+    # 將兩種誤差相加成總誤差
+    loss = lambda1*L  + lambda2*l
+
+  # 計算總損失對模型參數的梯度
+  g = tape.gradient(loss, model.trainable_weights)
+  # 使用優化器更新模型參數
+  opt.apply_gradients(zip(g, model.trainable_weights))
+
+
+  if epoch % 100 == 0 or epoch == epochs-1: # 每隔 100 個 epoch 或最後一個 epoch 記錄當前的損失值
+    print(f"{epoch:5}, {loss.numpy():.9f}")
+    loss_values = np.append(loss_values, loss)
+    L_values = np.append(L_values, L)
+    l_values = np.append(l_values, l)
+
+# 總計算時間
+end = time.time()
+computation_time = {}
+computation_time["pinn"] = end - start
+print(f"\ncomputation time: {end-start:.3f}\n")
+# 繪製總損失 loss 、PDE損失 L 和 邊界點誤差l 隨 epoch 的變化曲線
+print("-------------------------------")
+# ----------------------------對數畫法------------------------
+plt.figure()
+plt.semilogy(loss_values, label='total Loss')
+plt.xlabel("Epochs" r'($\times 10^2$)',fontsize=16)
+plt.legend()
+# ------------------------------------------------------------
+# plt.figure()
+# plt.plot(loss_values, label='total Loss')
+# plt.legend()
+# plt.xlabel("Epochs" r'($\times 10^2$)',fontsize=16)
+
+# 繪製邊界點誤差 l 和 PDE 殘差損失 L 隨 epoch 的變化曲線。
+# ----------------------------對數畫法------------------------
+plt.figure()
+# plt.legend()
+plt.semilogy(l_values, label='Loss_data')
+# plt.legend()
+
+plt.semilogy(L_values, label='Loss_PDE')
+plt.xlabel("Epochs" r'($\times 10^2$)',fontsize=16)
+plt.legend()
+#---------------------------------------------------------------
+# plt.figure()
+# plt.plot(l_values, label='Loss_data')
+# plt.plot(L_values, label='Loss_PDE')
+# plt.legend()
+# plt.xlabel("Epochs" r'($\times 10^2$)',fontsize=16)
+
+n = 100     #設置網格大小 n
+r = np.linspace(a, max_r, n)
+theta = np.linspace(0, np.pi, n)
+phi = np.linspace(0, 2*np.pi, 2*n)
+r, theta ,phi = np.meshgrid(r, theta, phi)
+
+r = r.reshape([2*n*n*n, 1])
+theta = theta.reshape([2*n*n*n, 1])
+phi = phi.reshape([2*n*n*n, 1])
+
+
+r_T = tf.convert_to_tensor(r)
+theta_T = tf.convert_to_tensor(theta)
+phi_T = tf.convert_to_tensor(phi)
+SS = true_solution(r_T,theta_T,phi_T)
+S = u(r_T,theta_T,phi_T)
+M=SS-S
+
+M=tf.reduce_mean(tf.abs(M))
+print("Absolute error: {}".format(M.numpy()))
+
+# plt.show()
+
+# 測試
+# 假设 x, y, true_solution(x, y) 是你的数据
+# 並將其座標攤平為 (nn, 1) 維張量 X 和 Y
+n = 100
+theta = np.linspace(0, np.pi, n)
+phi = np.linspace(0, 2*np.pi, 2*n)
+theta0 ,phi0 = np.meshgrid(theta, phi)
+theta = theta0.reshape([2*n*n, 1])
+phi = phi0.reshape([2*n*n, 1])
+# 將 theta 和 phi 轉換為 Tensorflow 張量
+theta_T = tf.convert_to_tensor(theta)
+phi_T = tf.convert_to_tensor(phi)
+
+
+# print(theta_T.shape)
+# print(phi_T.shape)
+ran=np.linspace(a,max_r,5)
+for i in ran:
+  r_T = tf.ones_like(theta_T)*i
+  R_T = u(r_T, theta_T, phi_T)
+  r0 = R_T.numpy().reshape(2*n, n)
+  # print(r0)
+  # print(phi0.shape)
+
+
+  X1 = r0 * np.sin(phi0) * np.cos(theta0)
+  Y1 = r0 * np.sin(phi0) * np.sin(theta0)
+  Z1 = r0 * np.cos(phi0)
+
+
+  r1 = np.abs(true_solution(np.ones((2*n, n))*i,theta0,phi0)-r0)
+  X2 = r1 * np.sin(phi0) * np.cos(theta0)
+  Y2 = r1 * np.sin(phi0) * np.sin(theta0)
+  Z2 = r1 * np.cos(phi0)
+
+
+  r2 = true_solution(np.ones((2*n, n))*i,theta0,phi0)
+  # print(true_solution(np.ones((2*n, n))*i,theta0,phi0))
+  # print(r0)
+  X3 = r2 * np.sin(phi0) * np.cos(theta0)
+  Y3 = r2 * np.sin(phi0) * np.sin(theta0)
+  Z3 = r2 * np.cos(phi0)
+
+  fig = plt.figure(figsize=(18, 6))
+
+  # # 左侧子图
+  ax2 = fig.add_subplot(131, projection='3d')
+  surf1 = ax2.plot_surface(X1, Y1, Z1, cmap='rainbow')
+  ax2.set_xlabel("x", fontsize=16)
+  ax2.set_ylabel("y", fontsize=16)
+  ax2.set_zlabel("z", fontsize=16)
+  ax2.set_title("PINN Solution (r={})".format(i), fontsize=16)
+
+  # 中間子图
+  ax3 = fig.add_subplot(132, projection='3d')
+  surf2 = ax3.plot_surface(X2, Y2, Z2, cmap='rainbow')
+  ax3.set_xlabel("x", fontsize=16)
+  ax3.set_ylabel("y", fontsize=16)
+  ax3.set_zlabel("z", fontsize=16)
+  ax3.set_title("Absolute error (r={})".format(i), fontsize=16)
+  # ax3.set_title("Solution (r={})".format(i), fontsize=16)
+
+  # 中間子图
+  ax4 = fig.add_subplot(133, projection='3d')
+  surf3 = ax4.plot_surface(X3, Y3, Z3, cmap='rainbow')
+  ax4.set_xlabel("x", fontsize=16)
+  ax4.set_ylabel("y", fontsize=16)
+  ax4.set_zlabel("z", fontsize=16)
+  ax4.set_title("Solution (r={})".format(i), fontsize=16)
+
+# plt.show()
+
+# 獲取所有圖像編號
+fig_nums = plt.get_fignums()
+
+# 使用迴圈保存所有圖像
+for i in fig_nums:
+    plt.figure(i)
+    plt.savefig(f'Laplace equation 3D_electromagnetism 運行結果/figure_{i}.png')
+
+# import numpy as np
+# n=3
+# # 定义 x 和 y 方向上的坐标值
+# x = np.array([1, 2, 3])
+# y = np.array([4, 5, 6, 7, 8, 9])
+
+# # 生成网格坐标矩阵
+# X0, Y0 = np.meshgrid(x, y)
+# X = X0.reshape([2*n*n, 1])
+# Y = Y0.reshape([2*n*n, 1])
+# Z = X*Y
+# Z0=Z.reshape([2*n, n])
+# print("X 坐标矩阵:")
+# print(X0)
+# print("\nY 坐标矩阵:")
+# print(Y0)
+# print(Z0)
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# # 生成極座標角度數據
+# theta = np.linspace(0, 2*np.pi, 1000)
+
+# # 設定極座標圖形的半徑
+# r = 2 + np.cos(3*theta)  # 一個玫瑰線的方程
+
+# # 繪製極座標圖
+# fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+# ax.plot(theta, r)
+# ax.set_title('Polar Plot', va='bottom')
+
+# # 設置角度刻度標籤
+# ax.set_xticks(np.linspace(0, 2*np.pi, 8, endpoint=False))
+# ax.set_xticklabels(['0', r'$\pi/4$', r'$\pi/2$', r'$3\pi/4$',
+#                     r'$\pi$', r'$5\pi/4$', r'$3\pi/2$', r'$7\pi/4$'])
+
+# plt.show()
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+
+# # 生成极坐标角度数据
+# theta = np.linspace(0, 2*np.pi, 1000)
+# phi = np.linspace(0, np.pi, 1000)
+# THETA, PHI = np.meshgrid(theta, phi)
+
+# # 设定极坐标图形的半径
+# R = 1 + 0.5*np.cos(3*THETA) + 0.5*np.sin(2*PHI)
+
+# # 将极坐标转换为笛卡尔坐标
+# X = R * np.sin(PHI) * np.cos(THETA)
+# Y = R * np.sin(PHI) * np.sin(THETA)
+# Z = R * np.cos(PHI)
+
+# # 创建一个三维的投影
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+
+# # 绘制点点图
+# ax.scatter(X.ravel(), Y.ravel(), Z.ravel(), s=1)
+
+# # 设置坐标轴标签
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+
+# # 设置图形标题
+# ax.set_title('3D Polar Plot')
+
+# plt.show()
+
+# # 生成極座標角度數據
+# theta = np.linspace(0, 2*np.pi, 1000)
+# phi = np.linspace(0, np.pi, 1000)
+# THETA, PHI = np.meshgrid(theta, phi)
+
+# # 設定極座標圖形的半徑
+# R = 1 + 0.5*np.cos(3*THETA) + 0.5*np.sin(2*PHI)
+
+# # 將極座標轉換為笛卡兒坐標
+# X = R * np.sin(PHI) * np.cos(THETA)
+# Y = R * np.sin(PHI) * np.sin(THETA)
+# Z = R * np.cos(PHI)
+
+# # 繪製三維極座標圖
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.plot_surface(X, Y, Z, rstride=5, cstride=5, cmap='viridis', edgecolor='none')
+# ax.set_title('3D Polar Plot')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
